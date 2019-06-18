53de7899b1ebbc4c7e4967bbcb668c6de99c00f0
Frank Treacy <me@frank06.net>
2019-06-18 11:55:45 -0500

    first commit

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function() {});
+Router.map(function() {
+  this.route('posts', function() {
+    this.route('post', { path: ':post_id' });
+  });
+});
 
 export default Router;
---
+++ mirage/factories/post.js
@@ -0,0 +1,18 @@
+import { Factory } from 'ember-cli-mirage';
+import faker from 'faker';
+
+export default Factory.extend({
+
+  title() {
+    return faker.lorem.sentence();
+  },
+
+  body() {
+    return faker.lorem.paragraph();
+  },
+
+  publishedAt() {
+    return faker.date.past();
+  }
+
+});
